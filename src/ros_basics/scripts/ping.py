c05839297fffd2ac1ac07fa3c5a31f1422d9bd1d
SandraSid <siglesiasd@gmail.com>
2022-03-01 23:54:09 +0100

    Fixim var_names errors v2

--- src/ros_basics/scripts/ping.py
+++ src/ros_basics/scripts/ping.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 ## ping_pong -> PING
 
 rospy.init_node("ping_node", anonymous=True)  		#Create a ping node
-pub = rospy.Publisher("ping_topic", String, queue_size=10) #Publish a String type message
+pub = rospy.Publisher("/ping_topic", String, queue_size=10) 	#Publish a String type message
 								#in /ping topic
 rate = rospy.Rate(1)						#Each second
 
@@ -13,6 +13,6 @@ rate = rospy.Rate(1)						#Each second
 
 while not rospy.is_shutdown():
 	msg = String			#Definetype of message
-	msg.data = "ping"		#Set message
+	msg.data = ping		#Set message
 	pub.publish(msg)		#Publish message
 	rate.sleep()			##Wait untill new
--- src/ros_basics/scripts/pong.py
+++ src/ros_basics/scripts/pong.py
@@ -4,15 +4,15 @@ from std_msgs.msg import String
 
 ## ping_pong -> PONG
 
-def pong(msg):
+def pong_exercice(msg):
 	global a
 	counter += msg.data
 	new_msg = String		#Define type of message
-	new_msg.data = "pong"		#Set message
+	new_msg.data = pong		#Set message
 	pub.publish(new_msg)		#Publish new message
-	rospy.loginfo("System answer: : %s", new_message)
+	rospy.loginfo("System answer: %s", new_message)
 
 rospy.init_node('pong_node')		#Creating a pong node
 pub = rospy.Publisher("pong_topic", String, queue_size=10)	
-sub = rospy.Subscriber("ping_topic", String, pong)
+sub = rospy.Subscriber("ping_topic", String, pong_exercice)
 rospy.spin()
